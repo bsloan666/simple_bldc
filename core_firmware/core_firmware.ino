05b7de604f327a9c0d047dc578fac1374a27772b
Blake Sloan <bsloan@marvelstudios.com>
2025-06-30 14:42:25 -0700

    working firmware

--- core_firmware/core_firmware.ino
+++ core_firmware/core_firmware.ino
@@ -1,6 +1,13 @@
 #include <PID_v1.h>
 
-double curr_encoder = 0;
+
+volatile int reverse = 0;
+volatile int aval;
+volatile int bval;
+volatile int direction_lookup[4] = {
+  -1, 1, 1, -1,
+};
+
 
 long index = 0;
 long prev_index = 0;
@@ -13,28 +20,30 @@ long pmeter;
 unsigned long interval = 20;
 long direction = -1;
 long prev_dir;
-long speed = 255;
-int phase;
-int prev_phase;
-int reverse = 0;
-int steps = 36;
-int aval;
-int bval;
-int req_pos;
+volatile long speed;
+int req_speed = 0;
 
-int SET = 10;
-int direction_lookup[4] = {
-  -1, 1, 1, -1,
-};
+int steps = 36;
+
+int req_pos;
+unsigned long timeout;
+unsigned long start_time;
+
+int SET_POS = 10;
+int SET_SPEED = 20;
+int SET_LOCK  = 30;
+int SET_STOP  = 40;
+
+
 class SimpleBLDCServo {
   private:
     unsigned int motor_a_pin;
     unsigned int motor_b_pin;
-    double previous_encoder_value;
+    int _lock;
     double target_position;
     double velocity;
     double target_velocity;
-    double output_position_delta, output_position_magnitude;  
+    double  output_position_magnitude;  
     double output_velocity_delta;
     bool encoder_changed; 
     PID position_pid;
@@ -43,11 +52,31 @@ class SimpleBLDCServo {
 
 
   public:
-    SimpleBLDCServo():
-      position_pid(&curr_encoder, &output_position_delta, &target_position, 5.0, 0.1, 0.03, DIRECT),
-      velocity_pid(&velocity, &output_velocity_delta, &target_velocity, 2.5, 0.0, 0.0, DIRECT)
-    {
+    double curr_encoder;
+    double prev_encoder;
+    double output_position_delta;
     
+    SimpleBLDCServo():
+      position_pid(&curr_encoder, &output_position_delta, &target_position, 0.85, 0.04, 0.07, DIRECT),
+      velocity_pid(&velocity, &output_velocity_delta, &target_velocity, 10.5, 0.0, 0.0, DIRECT)
+    {
+      curr_encoder = 0;
+      prev_encoder = 0;
+      _lock = 0;
+    }
+    void unlock(){
+      _lock = 0;
+       digitalWrite(5, LOW);
+       digitalWrite(6, LOW);
+    }
+    void lock(){
+      _lock = 1;
+       digitalWrite(5, LOW);
+       digitalWrite(6, HIGH);
+    }
+
+    int is_locked(){
+      return _lock;
     }
     int get_pos(){
       return curr_encoder;
@@ -79,12 +108,14 @@ class SimpleBLDCServo {
        target_velocity = init_targ_vel;
     }
     void poke(){
-        analogWrite(6, 255);
-        digitalWrite(5, LOW);
-        delay(15); 
-        digitalWrite(6, LOW);
-        analogWrite(5, 255);
-        delay(15);
+        unlock();
+        if(target_position > curr_encoder){
+          analogWrite(6, 255);
+          digitalWrite(5, LOW);
+        } else { 
+          digitalWrite(6, LOW);
+          analogWrite(5, 255);
+        }
     }
 
     void dump()
@@ -95,9 +126,21 @@ class SimpleBLDCServo {
       Serial.print("Target:");
       Serial.print(target_position);
       Serial.print(",");
+      Serial.print("Timeout:");
+      Serial.print(timeout);
+      Serial.print(",");
       Serial.print("Delta:");
       Serial.println(output_position_delta);
     }
+
+    void set_speed(long req_speed, unsigned long _timeout){
+      output_position_delta = 1;
+      if(req_speed < 0){
+        output_position_delta = -1;
+      }
+      speed = abs(req_speed);
+    }
+
     void set_to_off(){
       digitalWrite(motor_a_pin, LOW);
       digitalWrite(motor_b_pin, LOW);
@@ -106,7 +149,7 @@ class SimpleBLDCServo {
     void cycle()
     {
       encoder_changed = false;
-      velocity = abs(curr_encoder - previous_encoder_value);
+      velocity = int(abs(curr_encoder - prev_encoder));
  
       position_pid.Compute();
       velocity_pid.Compute();
@@ -114,16 +157,15 @@ class SimpleBLDCServo {
       output_position_magnitude = constrain(output_position_magnitude, 0, abs(output_velocity_delta));
       speed = output_position_magnitude;
      
-      reverse = output_position_delta < 0;
-
-      if(previous_encoder_value != curr_encoder){
+      if(velocity){
         encoder_changed = true;
       }
-      previous_encoder_value = curr_encoder;
+      prev_encoder = curr_encoder;
+      
     }
 };
 
-SimpleBLDCServo servo;
+volatile SimpleBLDCServo servo;
 
 
 void setup() {
@@ -133,20 +175,26 @@ void setup() {
   pinMode(2, INPUT);
   pinMode(3, INPUT);
   attachInterrupt(digitalPinToInterrupt(2), set_polarity, CHANGE);
-  // attachInterrupt(digitalPinToInterrupt(3), set_polarity, CHANGE);
+  // attachInterrupt(digitalPinToInterrupt(3), get_sensor, CHANGE);
   Serial.begin(9600);
   prev_time = millis();
   servo.init_pids(interval);
-  servo.init_targets(0.0, 64);
+  timeout = 0;
+  // servo.init_targets(0.0, 64);
 }
-
+//void get_sensor(){
+// bval = digitalRead(3);
+//}
 void set_polarity(){
+  if(servo.is_locked()){
+    return;
+  }
   aval = digitalRead(2);
   bval = digitalRead(3);
-  
   int dir = direction_lookup[aval * 2 + bval];
-  curr_encoder += dir;
-  //speed = 200;
+  servo.curr_encoder += dir;
+  reverse = servo.output_position_delta < 0;
+  
   if(reverse){
     if(aval == HIGH){
         analogWrite(6, speed);
@@ -155,7 +203,6 @@ void set_polarity(){
         digitalWrite(6, LOW);
         analogWrite(5, speed);
     }
-    index--;
   } else {
     if(aval == LOW){
         analogWrite(6, speed);
@@ -164,7 +211,6 @@ void set_polarity(){
         digitalWrite(6, LOW);
         analogWrite(5, speed);
     }
-    index++;
   }
 }
 
@@ -173,22 +219,35 @@ void loop() {
   if((curr_time - prev_time) >= interval){
     prev_time = curr_time;
     if(Serial.available()){ 
-        int cmd = Serial.parseInt();
-        if(cmd == SET){
-          req_pos = Serial.parseFloat();
-          speed = Serial.parseFloat();
-          servo.init_targets(req_pos, speed); 
-          servo.poke();
-        }
+      int cmd = Serial.parseInt();
+      if(cmd == SET_POS){
+        req_pos = Serial.parseFloat();
+        speed = Serial.parseFloat();
+        servo.init_targets(req_pos, speed);           
+        servo.poke();
+      }
+      if(cmd == SET_SPEED){
+        long req_speed =  Serial.parseInt();
+        timeout = Serial.parseInt();
+        start_time = curr_time;
+        servo.set_speed(req_speed, timeout);
+        servo.poke();
+      }
+      if(cmd == SET_LOCK){         
+        servo.lock();
+      }
     }
-    /*
-    if(index != prev_index){
+    if(timeout){
+      if(curr_time >= start_time + timeout){
+        servo.lock();
+        delay(40);
+        servo.unlock();
+        servo.init_targets(servo.curr_encoder, 255);   
+        timeout = 0;
+      }
       servo.dump();
-    }
-    */
-    prev_index = index;
-  
-    servo.cycle(); 
-    //servo.dump();
+    } else {
+      servo.cycle();
+    } 
   }
 }
